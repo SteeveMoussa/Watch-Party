8b523fdecbc036fe605612a0e6643cd6dc19c688
xdesign-steevemoussa <steeve.moussa@xdesign.com>
2024-03-26 11:41:29 +0000

    Removed template code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./Login"
+import Watchlist from "./Watchlist"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div>
+            Movie App    
+            <br />
+            <br />
+            <Login></Login>
+            {/* <Watchlist></Watchlist> */}
+        </div>
+    )
 }
 
-export default App;
+export default App
